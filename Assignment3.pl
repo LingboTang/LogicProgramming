167aeb51b84b5237016aefab538faadf1d38de38
Lingbo Tang <lingbo19.tang@gmail.com>
2015-03-17 10:20:08 -0600

    Yes

--- Assignment3.pl
+++ Assignment3.pl
@@ -1,5 +1,35 @@
+/*
+ Course: CMPUT325
+ Prof: You
+ Student: Lingbo Tang
+*/
 
 
+/*
+ Question 1
+ setUnion(+S1, +S2, -S3)
+ Given two lists S1 and S2, setUnion returns the union of 
+ those two lists in S3
+*/
+setUnion([], S3, S3).
+% if A is a member of S2, ignore it
+setUnion([A | Rest], S2, S3) :- member(A, S2), setUnion(Rest, S2, S3).
+% otherwise append it to the result
+setUnion([A | Rest], S2, [A | S3]) :- setUnion(Rest, S2, S3).
+
+
+
+/*
+Question 2
+swap(+S1,+S2)
+Given two sets S1 and S2, swap returns the
+swapped sets at certain points in the lists
+*/
+
+
+% Initial condition is both of the
+% sets have the same order of elements
+% or empty
 swap([], []).
 swap([A],[A]).
 swap(A, B) :-
---
+++ Classnote.pl
@@ -0,0 +1,20 @@
+xmember(A,[A|_]) :- !.
+xmember(A,[B|L]) :-xmember(A,L).
+
+rm(_,[],[]).
+rm(A,[A|L],R) :- !, rm(A,L,R).
+rm(A,[B|L],[B|R]) :- rm(A,L,R).
+
+r(W) :- rm(a,[a,b,a,d,a,l],W).
+
+sum([],0).
+sum([A|L],S) :- number(A),sum(L,S1),S.
+sum([A|L],S) :- atom(A),sum(L,S).
+sum([A|L],S) :- sum(A,S1),sum(L,S2),S.
+
+i :- consult('Classnote.pl').
+
+t0(I) :- sum([2,3,[5,6],7],I).
+t1(I) :- sum([2,3],I).
+t2(I) :- sum([2,a,3,[5,b],7,a],I).
+t3(I) :- sum([2,[],a,[],3,[5,b,6],7],I).
