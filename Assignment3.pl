28c5e920efec70c5103b87bb7232afb761019a5e
Lingbo Tang <lingbo19.tang@gmail.com>
2015-03-23 16:13:59 -0600

    Need to finish the Q6 and Comment

--- Assignment3.pl
+++ Assignment3.pl
@@ -65,10 +65,15 @@ flat([A|L], [A|L1]) :- flat(L,L1).
 rmAllDup(L,R) :- getridDup(L,[],R).
 
 getridDup([],_,[]).
-getridDup([A|L],S,[A|R]) :- atom(A), \+ member(A,S), getridDup(L,[A|S],R).
-getridDup([A|L],S,R) :- atom(A),member(A,S),getridDup(L,S,R).
-getridDup([A|L],S,[LA|R]) :- getridDup(A,S,LA), flat(A,FA),append(FA,S,S1), getridDup(L,S1,R). 
+getridDup([A|L],M,[A|R]) :- \+ is_list(A), \+ member(A,M), getridDup(L,[A|M],R).
+getridDup([A|L],M,R) :- \+ is_list(A),member(A,M),getridDup(L,M,R).
+getridDup([A|L],M,[B|R]) :- is_list(A), getridDup(A,M,B), flat(A,FA),append(FA,M,S1), getridDup(L,S1,R). 
 
+% is_list from http://www.swi-prolog.org/pldoc/man?predicate=is_list/1
+
+is_list(X) :- var(X), !, fail.
+is_list([]).
+is_list([_|T]) :- is_list(T).
 
 
 /*
@@ -79,17 +84,17 @@ getridDup([A|L],S,[LA|R]) :- getridDup(A,S,LA), flat(A,FA),append(FA,S,S1), getr
  otherwise Current remains the same
 */
 
-largest([A | Rest], N) :- largest_helper(Rest, N, A).
+largest([A|R], N) :- find_largest(R, N, A).
 
-largest_helper([], Out, Out).
-largest_helper([A | Rest], Out, Current) :- A > Current, largest_helper(Rest, Out, A).
-largest_helper([A | Rest], Out, Current) :- largest_helper(Rest, Out, Current).
+find_largest([], Out, Out).
+find_largest([A|R], Out, Current) :- A > Current, find_largest(R, Out, A).
+find_largest([A|R], Out, Current) :- find_largest(R, Out, Current).
 
-smallest([A | Rest], N) :- smallest_helper(Rest, N, A).
+smallest([A|R], N) :- find_smallest(R, N, A).
 
-smallest_helper([], Out, Out).
-smallest_helper([A | Rest], Out, Current) :- A < Current, smallest_helper(Rest, Out, A).
-smallest_helper([A | Rest], Out, Current) :- smallest_helper(Rest, Out, Current).
+find_smallest([], Out, Out).
+find_smallest([A|R], Out, Current) :- A < Current, find_smallest(R, Out, A).
+find_smallest([A|R], Out, Current) :- find_smallest(R, Out, Current).
 
 generate([A],_,B).
 generate(L,Choice,N) :- Choice == smallest, flat(L,K), smallest(K,N).
@@ -100,24 +105,35 @@ generate(L,Choice,N) :- Choice == largest, flat(L,K), largest(K,N).
 /*
  Question 5
 */
+countAll([],[]).
+countAll(L,R) :- flat(L,FL),rmDup(FL,G),form_pair(G,FL,I),insert_sort(I,Q),my_reverse(Q,R).
 
-countt(_,[],0).
-countt(A,[A|L],N):- !,countt(A,L,N1),N is N1+1.
-countt(A,[B|L],N):- is_list(B),countt(A,L,N),!.
-countt(A,[B|L],N):- countt(A,B,N1),countt(A,L,N2),N is N1+N2.
-
-
-% is_list from http://www.swi-prolog.org/pldoc/man?predicate=is_list/1
-
-is_list(X) :-
-        var(X), !,
-        fail.
-is_list([]).
-is_list([_|T]) :-
-        is_list(T).
+form_pair([],L,[]).
+form_pair([A|P],L,[[A,N]|R]) :- counter(A,L,N),form_pair(P,L,R).
 
 
+% This function does the work:
+% Given a list and a certain atom, find the count of the atom in the list.
+
+counter(_,[],0).
+counter(A,[A|L],N):- flat(L,K),!,counter(A,K,N1),N is N1+1.
+counter(A,[_|L],N):- flat(L,K),counter(A,K,N). 
+
+% Sort the result counter, this function is to sort the result returned
+% from countt.
+% This predicate is from 325 TA help session.
+
+insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+i_sort([],Acc,Acc).
+i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
+insert([O1,X],[[O2,Y]|T],[[O2,Y]|NT]):-X>Y,insert([O1,X],T,NT).
+insert([O1,X],[[O2,Y]|T],[[O1,X],[O2,Y]|T]):-X=<Y.
+insert([O1,X],[],[[O1,X]]).
+
+my_reverse([],[]).
+my_reverse([H|T],L):- my_reverse(T,R),append(R,[H],L).
 
 /*
  Question 6
 */
+
